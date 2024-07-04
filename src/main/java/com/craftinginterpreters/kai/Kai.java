5722ab93901b8709f399ce6fad6aedb921f95ca8
NgYiKai <62179279+NgYiKai@users.noreply.github.com>
2024-07-04 13:50:31 +0000

    implemented interpreter

--- src/main/java/com/craftinginterpreters/kai/Kai.java
+++ src/main/java/com/craftinginterpreters/kai/Kai.java
@@ -15,7 +15,9 @@
 
  public class Kai {
 
+  private static final Interpreter interpreter = new Interpreter();
   static boolean hadError = false;
+  static boolean hadRuntimeError = false;
 
   public static void main(String[] args) throws IOException {
     if (args.length > 1) {
@@ -31,7 +33,11 @@
   private static void runFile(String path) throws IOException {
     byte[] bytes = Files.readAllBytes(Paths.get(path));
     run(new String(bytes, Charset.defaultCharset()));
-    if (hadError) System.exit(65);
+
+    if (hadError)
+      System.exit(65);
+    if (hadRuntimeError)
+      System.exit(70);
   }
 
   private static void runPrompt() throws IOException {
@@ -58,7 +64,7 @@
     // Stop if there was a syntax error.
     if (hadError) return;
 
-    System.out.println(new AstPrinter().print(expression));
+    interpreter.interpret(expression);
   }
 
   static void error(int line, String message) {
@@ -79,6 +85,13 @@
       report(token.line, " at '" + token.lexeme + "'", message);
     }
   }
+
+  static void runtimeError(RuntimeError error) {
+    System.err.println(error.getMessage() +
+        "\n[line " + error.token.line + "]");
+    hadRuntimeError = true;
+  }
+
 }
 
  
